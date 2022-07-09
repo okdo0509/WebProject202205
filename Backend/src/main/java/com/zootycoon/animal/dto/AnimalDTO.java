d7fe3cf58e554267771cb4042ce1322d91299f85
okdo0509 <okdo0509@gmail.com>
2022-07-10 00:27:00 +0900

    동물농장 전체조회 기능
    - getMapping
    - 전체조회 기능
    - controller
    - service
    - repository
    - dto, vo, entity
    - mapsstruct

---
+++ Backend/src/main/java/com/zootycoon/animal/dto/AnimalDTO.java
@@ -0,0 +1,28 @@
+package com.zootycoon.animal.dto;
+
+ 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+ 
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class AnimalDTO {
+    
+    private String id;
+    private String species;
+    private String name;
+    private int age;
+    private String isAlive;
+
+ 
+
+}
